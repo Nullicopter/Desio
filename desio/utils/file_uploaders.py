c53341a3a3b3f0130f51930731ee1b0497429b70
dialtone <dialtone@adroll.com>
2011-04-03 11:08:01 -0700

    add support for s3

--- desio/utils/file_uploaders.py
+++ desio/utils/file_uploaders.py
@@ -28,14 +28,33 @@ class S3Uploader(object):
     """
     Upload files to specified S3 locations
     """
+    s3 = None
     def __init__(self, base_url):
         parsed = urlparse.urlsplit(base_url)
         self.bucket = parsed.netloc.split(".", 1)[0]
         self.base_path = parsed.path
-        self.s3 = object()
 
     def set_contents(self, local_filepath, remote_filepath):
         """
         Upload content to S3 in self.bucket at self.path
         """
+        from boto.s3.connection import S3Connection
+        from boto.s3.key import Key
 
+        if self.s3 is None:
+            self.aws_api_key = pylons.config['aws_api_key'].strip()
+            self.aws_secret_key = pylons.config['aws_secret_key'].strip()
+            self.bucket_name = pylons.config['aws_s3_bucket'].strip()
+            self.s3 = S3Connection(aws_access_key_id=self.aws_api_key,
+                                   aws_secret_access_key=self.aws_secret_key)
+        bucket = self.s3.get_bucket(self.bucket_name)
+
+        headers = {}
+        headers['Content-Type'] = "application/octet-stream"
+
+        if remote_filepath.startswith("/"):
+            remote_filepath = remote_filepath[1:]
+        k = Key(self.bucket)
+        k.key = remote_filepath
+        k.set_contents_from_filename(local_filepath, headers, replace=True)
+        k.set_acl('public-read')
