877b00735cc0725b4784b04dd838caaf34f37859
Ben Ogle <ogle.ben@gmail.com>
2011-04-17 13:50:28 -0700

    build js

---
+++ desio/public/j/build/controllers/index.js
@@ -0,0 +1,1 @@
+(function($){Q.DashPage=Q.Page.extend({run:function(){this._super.apply(this,arguments)}})})(jQuery);
---
+++ desio/public/j/build/controllers/invite.js
@@ -0,0 +1,1 @@
+(function($){Q.InvitePage=Q.Page.extend({run:function(){this._super.apply(this,arguments);this.form=this.$("form").RedirectForm({defaultData:{default_timezone:-(new Date).getTimezoneOffset()/60,mode:$("#mode").val()},resetInitially:true});$("#name").inputHint();if(this.form.val("mode")=="login_user")this.form.focusFirst()}})})(jQuery);
