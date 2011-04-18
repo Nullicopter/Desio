1bd14e6c257a745572295060bcdad9f47f07ac84
Ben Ogle <ogle.ben@gmail.com>
2011-04-17 23:31:24 -0700

    build js

--- desio/public/j/build/controllers/index.js
+++ desio/public/j/build/controllers/index.js
@@ -1,1 +1,1 @@
-(function($){Q.DashPage=Q.Page.extend({run:function(){this._super.apply(this,arguments)}})})(jQuery);
+(function($){Q.DashPage=Q.Page.extend({run:function(){this._super.apply(this,arguments)}});Q.FrontPage=Q.Page.extend({run:function(){var self=this;this._super.apply(this,arguments);$("#email").inputHint();this.form=$("#request-invite form").AsyncForm({validationOptions:{rules:{email:"required"},messages:{email:"An email address, please."}},onSuccess:function(data){$("#request-invite").hide();$("#has-invite").show()}})}})})(jQuery);
